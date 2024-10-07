3003505c1dce20830223bb1c4c16e370755d671b
Acrylus <theantonjoseph@gmail.com>
2024-10-07 20:51:18 +0800

    Conclude Update

--- views/pages/Conclusion.py
+++ views/pages/Conclusion.py
@@ -7,6 +7,6 @@ st.write("""In this analysis, we examined the top 10 streamed music tracks, focu
         effectively support their reach. Additionally, the bar chart comparing audio attributes such as Danceability, Valence, and Energy provided insights 
         into the musical qualities that resonate with listeners. We also explored the BPM (beats per minute) of each track, helping us understand 
         the tempo and energy levels that influence listener engagement. Lastly, the line chart showcased the rankings of these tracks across various platforms, 
-        with lower position numbers indicating stronger performance. Together, these visualizations offer a comprehensive overview of the factors contributing to 
-        the success of music tracks in today’s streaming landscape, equipping stakeholders in the music industry with valuable insights for informed decision-making.""")
+        these visualizations offer a comprehensive overview of the factors contributing to the success of music tracks in today’s streaming landscape, 
+        equipping stakeholders in the music industry with valuable insights for informed decision-making.""")
 
